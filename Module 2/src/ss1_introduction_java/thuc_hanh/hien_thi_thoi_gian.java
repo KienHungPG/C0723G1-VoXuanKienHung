a3676a26ed20c62c3d47927780f663dfbcc1fcb3
KienHungPG <voxkienhung@gmail.com>
2023-08-31 14:32:44 +0700

    update module 2

---
+++ Module 2/src/ss1_introduction_java/thuc_hanh/hien_thi_thoi_gian.java
@@ -0,0 +1,10 @@
+package ss1_introduction_java.thuc_hanh;
+
+import java.util.Date;
+
+public class hien_thi_thoi_gian {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
