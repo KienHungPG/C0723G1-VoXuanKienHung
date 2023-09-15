6dfb121d6ffd96630f52017547c4f61196c6a50e
KienHungPG <voxkienhung@gmail.com>
2023-09-15 17:23:53 +0700

    update bai tap

--- Module 2/src/ss10_DSA/bai_tap/trien_khai_arraylist/MyList.java
+++ Module 2/src/ss10_DSA/bai_tap/trien_khai_arraylist/MyList.java
@@ -1,10 +1,11 @@
 package ss10_DSA.bai_tap.trien_khai_arraylist;
 
+import java.util.Arrays;
+
 public class MyList<E> {
     private int size = 0;
     static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
-    private java.util.Arrays Arrays;
 
     public MyList() {
     }
@@ -13,7 +14,7 @@ public class MyList<E> {
         elements = new Object[capacity];
     }
 
-    private void ensureCapa() {
+    private void ensureCapacity() {
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
@@ -41,16 +42,63 @@ public class MyList<E> {
 
     public void add(E e) {
         if (size == elements.length) {
-            ensureCapa();
+            ensureCapacity();
         }
         elements[size++] = e;
+    }
+
+    public void add(int index, E e) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index" + index + "out of bounds");
+        } else {
+            for (int i = size - 1; i >= index; i--) {
+                elements[i + 1] = elements[i];
+            }
+            elements[index] = e;
+            size++;
+        }
+    }
+
+    public Object clone() {
+        MyList<E> temp = new MyList<>();
+        temp.size = size;
+        temp.elements = Arrays.copyOf(elements, size);
+        return temp;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (e.equals(elements[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(E e) {
+        for (int i = 0; i < size; i++) {
+            if (e.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void clear() {
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
     }
 
     public String toString() {
         StringBuilder result = new StringBuilder("[");
         for (int i = 0; i < size; i++) {
             result.append(elements[i]);
-            if (i < size - 1) result.append(", ");
+            if (i < size - 1){
+                result.append(", ");
+            }
         }
         return result.toString() + "]";
     }
--- Module 2/src/ss10_DSA/bai_tap/trien_khai_arraylist/MyListTest.java
+++ Module 2/src/ss10_DSA/bai_tap/trien_khai_arraylist/MyListTest.java
@@ -1,19 +1,30 @@
 package ss10_DSA.bai_tap.trien_khai_arraylist;
 
+import java.util.List;
+
 public class MyListTest {
     public static void main(String[] args) {
-        MyList<String> myList = new MyList(6);
-        myList.add("Student1");
-        myList.add("Student2");
-        myList.add("Student3");
-        myList.add("Student4");
-        System.out.println(myList.get(0));
-        System.out.println(myList.get(1));
-        System.out.println(myList.get(2));
-        System.out.println(myList.get(3));
+        MyList<String> myList = new MyList<>(6);
+        MyList<String> myList2;
+        myList.add("1");
+        myList.add("2");
+        myList.add("4");
+        myList.add("5");
+        myList.add("6");
+        myList.add("7");
+        myList.add(2,"3");
+        System.out.println(myList);
         System.out.println("Phần tử bị xóa đi " +myList.get(2));
         myList.remove(2);
         System.out.println("Mảng sau khi xóa");
         System.out.println(myList);
+
+        myList2 = (MyList<String>) myList.clone();
+        System.out.println(myList2);
+        System.out.println(myList.contains("3"));
+        System.out.println(myList.indexOf("4"));
+        myList2.clear();
+        System.out.println(myList2);
+
     }
 }
