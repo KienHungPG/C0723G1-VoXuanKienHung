d9b7f657d40de846adcf3d6744b89e2d1d7d1e31
KienHungPG <voxkienhung@gmail.com>
2023-09-18 18:22:27 +0700

    Update Bracket.java

--- Module 2/src/ss11_Stack_Queue/bai_tap/Bracket.java
+++ Module 2/src/ss11_Stack_Queue/bai_tap/Bracket.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Bracket {
-    Stack<String> stack = new Stack<>();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
 
+        System.out.print("Enter expression: ");
+        String string = scanner.nextLine();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '(') {
+                stack.push(string.charAt(i));
+            }
+            if (string.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return;
+                } else if (stack.pop() != '(') {
+                    return;
+                }
+            }
+        }
+        System.out.println(stack.isEmpty() ? "Well" : "No");
+    }
 }
