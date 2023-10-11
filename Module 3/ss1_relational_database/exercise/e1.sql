4cb83e5cb7290c1b879798f4e382a6826ba0532b
KienHungPG <voxkienhung@gmail.com>
2023-10-11 15:38:44 +0700

    update bai tap

--- Module 3/ss1_relational_database/exercise/e1.sql
+++ Module 3/ss1_relational_database/exercise/e1.sql
@@ -1,18 +1,22 @@
-create database studentmanagement;
-use studentmanagement;
-create table Student(
-Id int,
-Name varchar(50),
-Age int,
-Country varchar(50)
+create database student_management;
+
+use student_management;
+
+create table student(
+student_id int,
+student_name varchar(50),
+student_age int,
+student_country varchar(50)
 );
-create table Class(
-Id int,
-Name varchar(50)
+
+create table class(
+class_id int,
+class_name varchar(50)
 );
-create table Teacher(
-Id int,
-Name varchar(50),
-Age int,
-Country varchar(50)
+
+create table teacher(
+teacher_id int,
+teacher_name varchar(50),
+teacher_age int,
+teacher_country varchar(50)
 );
