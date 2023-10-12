d8521f79ecf57fd67b1e2d8f7f158688efbd74e3
KienHungPG <voxkienhung@gmail.com>
2023-10-12 10:09:54 +0700

    update bai tap

--- Module 3/ss2_design_and_create_database/exercise/ex2/ex2.sql
+++ Module 3/ss2_design_and_create_database/exercise/ex2/ex2.sql
@@ -6,31 +6,36 @@ use quan_ly_ban_hang;
 
 create table customers(
 customer_id int auto_increment,
-customer_name varchar(50),
-customer_age int,
+customer_name varchar(50) not null,
+customer_age int not null,
 primary key(customer_id)
 );
 
 create table orders(
 customer_id int,
 order_id int auto_increment,
-order_date datetime,
-order_total_price int,
+order_date datetime not null, 
+order_total_price int not null,
+check (order_total_price >= 0),
 primary key(order_id),
 foreign key (customer_id) references customers(customer_id)
 );
 
 create table products(
 product_id int auto_increment,
-product_name varchar(50),
-product_price int,
+product_name varchar(50) not null,
+product_price int not null,
+check (product_price >= 0),
 primary key(product_id)
 );
 
 create table order_detail(
-order_id int,
-product_id int,
-order_qty varchar(50),
+order_id int not null,
+product_id int not null,
+order_quantity varchar(50) not null,
+check (order_quantity > 0),
+primary key(order_id,product_id),
 foreign key (order_id) references orders(order_id),
 foreign key (product_id) references products(product_id)
 );
+
