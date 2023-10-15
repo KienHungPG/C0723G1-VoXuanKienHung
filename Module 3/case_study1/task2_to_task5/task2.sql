d63c32f575345556028997210a347372e4968688
KienHungPG <voxkienhung@gmail.com>
2023-10-15 16:51:20 +0700

    update cs

--- Module 3/case_study1/task2_to_task5/task2.sql
+++ Module 3/case_study1/task2_to_task5/task2.sql
@@ -1,5 +1,7 @@
 -- Task 2 -- 
 
+/* Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự. */
+
 use furama_resort;
 
 select * from nhan_vien
--- Module 3/case_study1/task2_to_task5/task3.sql
+++ Module 3/case_study1/task2_to_task5/task3.sql
@@ -1,4 +1,5 @@
 -- Task 3 --
+/* Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”. */
 
 use furama_resort;
 
