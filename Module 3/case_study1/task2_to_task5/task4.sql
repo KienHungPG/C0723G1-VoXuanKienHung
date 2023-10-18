ee8dec4f61ea985b6d84bc46f98eba36914240bb
KienHungPG <voxkienhung@gmail.com>
2023-10-18 11:28:45 +0700

    update cs

--- Module 3/case_study1/task2_to_task5/task4.sql
+++ Module 3/case_study1/task2_to_task5/task4.sql
@@ -9,6 +9,7 @@ use furama_resort;
 select kh.ho_ten, count(hd.ma_khach_hang) as so_lan_dat_phong
 from khach_hang kh
 left join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
-where kh.ma_loai_khach = 1
+left join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+where lk.ten_loai_khach = 'Diamond'
 group by kh.ho_ten
 order by so_lan_dat_phong asc;
